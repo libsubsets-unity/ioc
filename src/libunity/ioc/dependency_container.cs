d174cc2b7b46da1e17bc5cb5d9e4b624f28bfdd0
maayalee <maayalee@gmail.com>
2017-04-13 07:01:03 +0900

    Refactoring

--- src/libunity/ioc/dependency_container.cs
+++ src/libunity/ioc/dependency_container.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 namespace libunity.ioc {
   public class dependency_container : ioc_container_base, service_locator_base,
     dependency_injector_base {
+
     public void register(object dep) {
       instances.Add(dep.GetType(), dep);
     }
@@ -24,30 +25,23 @@ namespace libunity.ioc {
       foreach (FieldInfo field in fields) {
         if (Attribute.IsDefined(field, typeof(dependency))) {
           object dependency = resolve(field.FieldType);
-          if (dependency != null)
+          if (dependency != null) {
             field.SetValue(instance, dependency);
-          else
+          }
+          else {
             throw new Exception(type.FullName + " Can't find dependeny " +
               field.FieldType.FullName);
+          }
         }
       }
     }
 
-    private FieldInfo[] get_type_info(object instance) {
-      Type type = instance.GetType();
-      return type.GetFields(BindingFlags.NonPublic |
-            BindingFlags.Public | BindingFlags.Instance);
-    }
-
     public T resolve<T>() {
       return (T)resolve(typeof(T));
-
     }
 
     public object resolve(Type type) {
-      if (instances.Contains(type))
-        return instances[type];
-      return null;
+      return instances.Contains(type) ? instances[type] : null;
     }
 
     private Hashtable instances = new Hashtable();
